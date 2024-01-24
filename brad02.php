8a2a4a5050815d12ed2d262761903bd302733ef1
Brad Chao <brad@brad.tw>
2024-01-24 11:25:50 +0800

    01241125

---
+++ brad02.php
@@ -0,0 +1,9 @@
+<h1>Brad Big Company</h1>
+<hr />
+<div>Hello, World</div>
+<?php
+    echo "Hello, Brad1<br />\n";                              
+    echo "Hello, Brad2<br />\n";
+    echo "Hello, Brad3<br />\n";
+?>
+<hr />
