067472b4da2f07ef3ee24b82f806b318a5e1003d
Brad Chao <brad@brad.tw>
2024-01-24 15:31:02 +0800

    01241531

--- brad07.php
+++ brad07.php
@@ -1,19 +1,31 @@
 <?php
-    $x = $y = $result = '';
+    $op = $x = $y = $result = '';
     if (isset($_GET['x']) && isset($_GET['y'])){
         $x = $_GET['x'];
         $y = $_GET['y'];
-        $result = $x + $y;
+        $op = $_GET['op'];
+        switch($op){
+            case '1': $result = $x + $y; break;
+            case '2': $result = $x - $y; break;
+            case '3': $result = $x * $y; break;
+            case '4': 
+                $r1 = (int)($x / $y);
+                $r2 = $x % $y;
+                $result = "{$r1} ...... {$r2}"; 
+                break;
+        }
+
+        
         //echo "{$x} + {$y} = {$result}";    
     }
 ?>
 <form>
     <input name="x" value='<?php echo $x; ?>' />
     <select name='op'>
-        <option value='1'>加</option>
-        <option value='2'>減</option>
-        <option value='3'>乘</option>
-        <option value='4'>除</option>
+        <option value='1' <?php echo $op=='1'?'selected':''; ?> >加</option>
+        <option value='2' <?php echo $op=='2'?'selected':''; ?> >減</option>
+        <option value='3' <?php echo $op=='3'?'selected':''; ?>>乘</option>
+        <option value='4' <?php echo $op=='4'?'selected':''; ?>>除</option>
     </select>
     <input name="y" value='<?php echo $y; ?>' />
     <input type="submit" value="=" />
