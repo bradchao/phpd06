46c0a59cd86cf0317b2f563c8542d21b26683a12
Brad Chao <brad@brad.tw>
2024-01-25 11:00:11 +0800

    01251100

--- brad13.php
+++ brad13.php
@@ -1,14 +1,32 @@
+<?php
+    $start = 1; $rows = 1; $cols = 9;
+    if (isset($_GET['start'])){
+        $start = $_GET['start'];
+        $rows = $_GET['rows'];
+        $cols = $_GET['cols'];
+    }
+?>
 <h1>Brad Big Company</h1>
 <hr />
+<form>
+    Start: <input type="number" name="start" />
+    Rows: <input type="number" name="rows" />
+    Cols: <input type="number" name="cols" />
+    <input type="submit" value="go" />
+</form>
 <hr />
 <table border="1" width="100%">
 
 <?php
-    for ($k = 0; $k < 2; $k++){
+    define('ROWS', $rows);
+    define('COLS',$cols );
+    define('START', $start);
+
+    for ($k = 0; $k < ROWS; $k++){
         echo '<tr>';
-        for ($j = 2; $j<= 5; $j++){
-            $newj = $j + $k * 4;
-            echo '<td>';
+        for ($j = START; $j< (START+COLS); $j++){
+            $newj = $j + $k * COLS;
+            echo '<td bgcolor=yellow>';
             for ($i = 1; $i <= 9; $i++){
                 $r = $newj * $i;
                 echo "{$newj} x {$i} = {$r}<br />";
