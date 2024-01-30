96a4aa90be7c26d5425d1bec8af8ccbbe1abb285
Brad Chao <brad@brad.tw>
2024-01-30 16:29:46 +0800

    01301630

--- brad34.php
+++ brad34.php
@@ -1,5 +1,8 @@
 <?php
-    $mysqli = new mysqli('localhost','root','');
-    $mysqli->query('CREATE DATABASE iii');
+    $mysqli = new mysqli('localhost','root','', 'iii');
+    $mysqli->set_charset('utf8');
+
+    $sql = 'INSERT INTO gift (name,addr) VALUES ("brad","abcdefg")';
+    $mysqli->query($sql);
 
 ?>
