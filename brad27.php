7f8aaeb6d8f0ccdc32976da14f94bb3bc0c3b339
Brad Chao <brad@brad.tw>
2024-01-30 10:54:59 +0800

    01301054

--- brad27.php
+++ brad27.php
@@ -1,7 +1,12 @@
 <?php
+    $dir = 'dir2/dir3/dir4';
+    if (!file_exists($dir)){
+        mkdir($dir, 0777, true);
+    }
+
     $data = "Hello, World\n1234567\n7654321\nabcdefg";
-    $fp = fopen('dir1/file1.txt', 'w');
-    fwrite($fp, $data);
+    $fp = fopen("{$dir}/file1.txt", 'a');
+    fwrite($fp, "Brad\n");
     fflush($fp);
     fclose($fp);
 
