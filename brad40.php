b83a561430c4c51de53da619e4b6b5de722394d3
Brad Chao <brad@brad.tw>
2024-01-31 13:59:45 +0800

    01311400

--- brad40.php
+++ brad40.php
@@ -1,18 +1,29 @@
 <?php
+    $rpp = 10;
+    $page = 1;
+    if (isset($_GET['page'])) $page = $_GET['page'];
+    $start = ($page - 1) * $rpp;
+    $prev = $page - 1;
+    $next = $page + 1;
+
     $mysqli = new mysqli('localhost','root','', 'iii');
     $mysqli->set_charset('utf8');
     
-    $sql = 'SELECT id,name,addr,city,town,picurl,feature FROM gift LIMIT 14, 7';
-    if (isset($_GET['key'])){
+    $sql = 'SELECT id,name,addr,city,town,picurl,feature FROM gift';
+    if (isset($_GET['key']) && strlen($_GET['key']) > 0){
         $key = $_GET['key'];
         $key2 = "%{$key}%";
 
         $sql .= ' WHERE name LIKE ? OR addr LIKE ? OR feature LIKE ?';
+        $sql .= ' LIMIT ?, ?';
+
         $stmt = $mysqli->prepare($sql);
-        $stmt->bind_param('sss', $key2, $key2, $key2);
+        $stmt->bind_param('sssii', $key2, $key2, $key2, $start, $rpp);
     }else{
+        $sql .= ' LIMIT ?, ?';
         $key= '';
         $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param('ii', $start, $rpp);
     }
     
     if ($stmt->execute()){
@@ -25,7 +36,8 @@
     <input type="submit" value="Search" />
 </form>
 <hr />
-<a href='?page=?' >Prev</a> | <a href='?page=?' >Next</a> 
+<a href='?page=<?php echo $prev; ?>&key=<?php echo $key; ?>' >Prev</a> | 
+<a href='?page=<?php echo $next; ?>&key=<?php echo $key; ?>' >Next</a> 
 <hr />
 <table border="1" width='100%'>
     <tr>
