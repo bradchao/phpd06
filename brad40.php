644e3f1a27ffa814e80d52c9320199d1c8fe5d05
Brad Chao <brad@brad.tw>
2024-01-31 12:01:53 +0800

    01311201

--- brad40.php
+++ brad40.php
@@ -2,17 +2,36 @@
     $mysqli = new mysqli('localhost','root','', 'iii');
     $mysqli->set_charset('utf8');
     
-    $sql = 'SELECT id,name,addr,city,town,picurl FROM gift';
-    $stmt = $mysqli->prepare($sql);
+    $sql = 'SELECT id,name,addr,city,town,picurl,feature FROM gift LIMIT 14, 7';
+    if (isset($_GET['key'])){
+        $key = $_GET['key'];
+        $key2 = "%{$key}%";
+
+        $sql .= ' WHERE name LIKE ? OR addr LIKE ? OR feature LIKE ?';
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param('sss', $key2, $key2, $key2);
+    }else{
+        $key= '';
+        $stmt = $mysqli->prepare($sql);
+    }
+    
     if ($stmt->execute()){
         $stmt->store_result();
-        $stmt->bind_result($id,$name,$addr,$city,$town,$url);
+        $stmt->bind_result($id,$name,$addr,$city,$town,$url,$feature);
 
 ?>
+<form>
+    Keyword:<input name="key" value='<?php echo $key; ?>' />
+    <input type="submit" value="Search" />
+</form>
+<hr />
+<a href='?page=?' >Prev</a> | <a href='?page=?' >Next</a> 
+<hr />
 <table border="1" width='100%'>
     <tr>
         <th>#</th>
         <th>Name</th>
+        <th>Feature</th>
         <th>Address</th>
         <th>City</th>
         <th>Town</th>
@@ -23,6 +42,7 @@
             echo '<tr>';
             echo "<td>{$id}</td>";
             echo "<td>{$name}</td>";
+            echo "<td>{$feature}</td>";
             echo "<td>{$addr}</td>";
             echo "<td>{$city}</td>";
             echo "<td>{$town}</td>";
